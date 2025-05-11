27483a673f8b0d3ae9c33af617d64d8dfd12e72a
MaelitoP <makibeardy@gmail.com>
2025-05-11 18:04:32 +0200

    chore: add github token in nix.conf

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,8 +1,28 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  githubTokenPath = "${config.xdg.configHome}/secrets/github_token";
+  nixTokenFile = "${config.xdg.configHome}/nix/access-tokens.conf";
+in
 {
-  nix.package = lib.mkForce pkgs.nixVersions.latest;
   nix.settings = {
     use-xdg-base-directories = true;
     experimental-features = "nix-command flakes";
   };
+
+  nix.extraOptions = ''
+    !include ${nixTokenFile}
+  '';
+
+  sops.secrets.github_token = {
+    sopsFile = ../secrets/default.yaml;
+    path = githubTokenPath;
+  };
+
+  home.activation.createNixAccessToken = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    install -d -m 0700 ${config.xdg.configHome}/nix
+    echo "access-tokens = github.com=$(<${githubTokenPath})" > ${nixTokenFile}
+    chmod 0600 ${nixTokenFile}
+  '';
 }
+
